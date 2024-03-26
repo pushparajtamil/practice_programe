7402ae7846683ace2e84d617c99704ec690b01c7
pushparajtamil <pushparaj11653@gmail.com>
2024-03-26 21:18:10 +0530

    commit tuple ex file

---
+++ TupleExample.py
@@ -0,0 +1,18 @@
+x=("apple","banana","kiwi")
+y=list(x)
+y.append("I Love Tamil")
+x=tuple(y)
+print(x)
+
+for i in x:
+    print(i)
+i=0
+while i <2:
+    print(i) 
+    i=i+1 
+a=["pushpa","tamil"]
+b=list(("love","love"))
+
+print(a+b)    
+
+
